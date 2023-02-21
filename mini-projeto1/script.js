5b38640e7ec68155740e72210e66ef8e5058d9ef
Emilly Mello <emill.ymello212@gmail.com>
2023-02-21 16:19:27 -0300

    Mini projeto 1: Trocando Valores de Variaveis

---
+++ mini-projeto1/script.js
@@ -0,0 +1,14 @@
+//Mini projeto 1: Trocando Valores de Variaveis
+
+let a = 'vermelho';
+let b = 'azul';
+
+console.log('a = ', a);
+console.log('b = ', b);
+
+let c = b;
+b = a;
+a = c;
+
+console.log('b = ', b);
+console.log('a = ', a);
